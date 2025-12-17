6ff782d8983a02574e2325421855c4d7cc70db63
Narendar Reddy Pandiri <narendarreddypandiri@email.com>
2025-12-17 00:35:15 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads requirements.txt and returns a list of dependencies
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="healthcare-mlops-diabetes",
+    version="0.0.1",
+    author="Narendar Reddy Pandiri",
+    author_email="narendar0314@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
